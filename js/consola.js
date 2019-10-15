80c4f628e946696e353765fd5c0e8bec56e26de5
Adri <adri14fg@gmail.com>
2019-10-15 19:34:36 +0200

    Ejercicio 1

---
+++ js/consola.js
@@ -0,0 +1,3 @@
+alert("Hola mundo!\nQué facil es incluir 'comillas simples'\ny \"comillas dobles\""); //Salida por ventana
+/*document.write("Hola mundo");> //Salida HMTL
+console.log("Hola mundo");*/ //Salida por consola
